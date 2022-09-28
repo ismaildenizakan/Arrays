06fdbd57310787b7c3ccf4aad76dffffd9545f35
Ismail Deniz AKAN <ismaildenizakan@gmail.com>
2022-09-28 23:08:41 +0300

    Arrays Project has been initialized.

--- Arrays/Arrays/src/Day012/MainApp.java
+++ Arrays/Arrays/src/Day012/MainApp.java
@@ -2,6 +2,27 @@ package Day012;
 
 public class MainApp {
     public static void main(String[] args) {
+        //Array (dizi) nasıl tanımlanır?
+        int[] sayilar=new int[5];
+        sayilar[0]=15;
+        sayilar[1]=24;
+        sayilar[2]=12;
+        sayilar[3]=7;
+        sayilar[4]=23;
+
+    int toplam=0;
+        for (int i = 0; i < sayilar.length; i++) {
+            toplam=toplam+sayilar[i];
+        }
+        double ort=(double) toplam/sayilar.length;
+        System.out.println("Toplam ="+toplam);
+        System.out.println("Ortalama ="+ort);
+
+        String[] futbolcular=new String[3];
+        String[] days=new String[7];
+        String[] months=new String[12];
+
+
 
     }
 }
---
+++ Arrays/Arrays/src/day013/ArraysApp.java
@@ -0,0 +1,7 @@
+package day013;
+
+public class ArraysApp {
+    public static void main(String[] args) {
+
+    }
+}
